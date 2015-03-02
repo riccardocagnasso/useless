8ae70b3a6c9d43fd843adec4daccf9e844fd8736
Riccardo Cagnasso <riccardo@phascode.org>
2015-03-02 15:19:34 +0100

    stub main

--- src/useless.py
+++ src/useless.py
@@ -1,2 +1,31 @@
+import argparse
+from useless.elf.structures import *
+
+
+def get_argument_parser():
+    parser = argparse.ArgumentParser(
+        prog='useless',
+        description='Useless is useless. Oh yeah, and parses bit and pieces' +
+        'of ELF and PE dynamic libraries')
+
+    parser.add_argument('library', type=str,
+                        help='library to parse')
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_argument_parser()
+
+    print(args.library)
+
+    stream = open(args.library, 'rb')
+
+    header = ELF_Header(stream)
+
+    for s in header.sections:
+        print(s.name)
+        print(s)
+
 if __name__ == "__main__":
     main()
